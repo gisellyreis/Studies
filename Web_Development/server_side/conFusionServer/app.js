3bc396a8c6deb2cfc09cd5623a37f004947a0df1
gisellyreis <gisellyareis@gmail.com>
2020-09-02 14:36:59 -0300

    authentication with passport

--- Web_Development/server_side/conFusionServer/app.js
+++ Web_Development/server_side/conFusionServer/app.js
@@ -6,6 +6,8 @@ var logger = require("morgan");
 const mongoose = require("mongoose");
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -34,6 +36,8 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // app.use(cookieParser("12345-67890-09876-54321"));
 app.use(session({
@@ -48,21 +52,15 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
-      var err = new Error("You are not authenticated!");
-      err.status = 401;
-      return next(err);
-  } else {
-    if(req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
+  }
+  else {
+        next();
   }
 }
 

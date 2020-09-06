8f76a10bc0798f85f35beca3c56f0da411541b14
gisellyreis <gisellyareis@gmail.com>
2020-09-05 23:16:27 -0300

    Activity Indicator

--- Web_Development/react_native/confusion/src/components/AboutComponent.js
+++ Web_Development/react_native/confusion/src/components/AboutComponent.js
@@ -5,6 +5,7 @@ import { FlatList } from "react-native";
 import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = (state) => {
   return {
@@ -56,32 +57,44 @@ class About extends Component {
       );
     };
 
-    return (
-      <>
+    if (this.props.leaders.isLoading) {
+      return(
           <ScrollView>
-          <History />
-          <Card featuredTitle="Corporate Leadership">
-            <Text
-              style={{
-                fontWeight: "bold",
-                display: "flex",
-                alignContent: "center",
-                alignSelf: "center",
-              }}
-            >
-              Corporate Leadership
-            </Text>
-
-            <FlatList
-              data={this.props.leaders.leaders}
-              renderItem={renderLeaders}
-              keyExtractor={(item) => item.id.toString()}
-            />
-          </Card>
+              <History />
+              <Card
+                  title='Corporate Leadership'>
+                  <Loading />
+              </Card>
           </ScrollView>
-      </>
-    );
+      );
   }
+  else if (this.props.leaders.errMess) {
+      return(
+          <ScrollView>
+              <History />
+              <Card
+                  title='Corporate Leadership'>
+                  <Text>{this.props.leaders.errMess}</Text>
+              </Card>
+          </ScrollView>
+      );
+  }
+  else {
+      return(
+          <ScrollView>
+              <History />
+              <Card
+                  title='Corporate Leadership'>
+              <FlatList 
+                  data={this.props.leaders.leaders}
+                  renderItem={renderLeader}
+                  keyExtractor={item => item.id.toString()}
+                  />
+              </Card>
+          </ScrollView>
+      );
+  }
+}
 }
 
 export default connect(mapStateToProps)(About);
--- Web_Development/react_native/confusion/src/components/MenuComponent.js
+++ Web_Development/react_native/confusion/src/components/MenuComponent.js
@@ -4,6 +4,7 @@ import { ListItem, Tile } from 'react-native-elements';
 
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -28,13 +29,27 @@ class Menu extends Component {
             );
         }
 
-        return (
-            <FlatList 
+        if (this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return(
+                <View>            
+                    <Text>{props.dishes.errMess}</Text>
+                </View>            
+            );
+        }
+        else {
+            return (
+                <FlatList 
                     data={this.props.dishes.dishes}
                     renderItem={renderMenuItem}
                     keyExtractor={item => item.id.toString()}
                     />
-        );
+            );
+        }
     }
 }
 
